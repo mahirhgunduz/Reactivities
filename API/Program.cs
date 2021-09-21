42d1d3ab44d3268b1849924b761b6c5bac494a79
Mahir Gündüz <mahir.gunduz@bimar.com.tr>
2021-09-21 14:13:06 +0300

    End of section 2

---
+++ API/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Persistence;
+using Persistence.Persistence;
+
+namespace API
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+           var host  = CreateWebHostBuilder(args).Build();
+
+            var scope = host.Services.CreateScope();
+
+           var services = scope.ServiceProvider;
+
+            try
+            {
+                var context = services.GetRequiredService<DataContext>();
+
+                await context.Database.MigrateAsync();
+
+                await Seed.SeedData(context);
+            }
+            catch (System.Exception ex)
+            {
+                
+               var logger = services.GetRequiredService<ILogger<Program>>();
+
+               logger.LogError(ex,"ERORRRORORR");
+            }
+
+             await host.RunAsync();
+           
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
---
+++ API/Startup.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Persistence;
+
+namespace API
+{
+    // public interface InterfaceDeneme {  
+    //     void Metod();
+    // }
+
+    // public class InterfaceDenemeClass : InterfaceDeneme
+    // {
+    //     public void Metod()
+    //     {
+    //         Console.WriteLine("HOOPPPP");
+    //     }
+    // }
+
+    public class Startup
+    {
+
+        private IConfiguration _config;
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        
+            // services.AddDbContext<DataContext>(opt => {
+            //     opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
+            // });
+
+                services.AddDbContext<DataContext>(opt => {
+                opt.UseSqlServer(_config.GetConnectionString("DefaultConnection"));
+            });
+
+            // services.AddTransient<InterfaceDeneme,InterfaceDenemeClass>();
+
+            // var serviceProvider = services.BuildServiceProvider();
+
+            // var inte = serviceProvider.GetService<InterfaceDeneme>();
+
+            // inte.Metod();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            //app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+
+    }
+}
